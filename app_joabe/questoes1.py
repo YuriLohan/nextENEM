ad30ad0679e723d89d80e81d95b10108b35bfdb3
Yurii <yurilohan1000@gmail.com>
2026-03-13 16:30:24 -0300

    add app_joabe

---
+++ app_joabe/questoes1.py
@@ -0,0 +1,420 @@
+questoes = [
+    {
+    "pergunta": """Leia o texto a seguir:
+
+    A importância da leitura de alto nível
+
+    A leitura de alto nível é a nossa ferramenta mais poderosa para
+    o pensamento analítico e crítico. Os signatários deste manifesto
+    pedem o reconhecimento da relevância permanente da leitura de
+    alto nível na era digital.
+
+    Em um ambiente de informação cada vez mais complexo, os
+    cidadãos informados precisam ser capazes de distinguir fontes
+    válidas de não válidas e ajustar de maneira flexível o comportamento
+    de leitura a contextos variados. O ato de leitura de alto nível é um
+    exercício de atenção e paciência cognitiva, expandindo vocabulários
+    e capacidades conceituais, desafiando ativamente as preconcepções
+    dos leitores. São especialmente os textos de formato longo, como
+    livros, que aprimoram nossas habilidades de leitura.
+
+    Portanto, pedimos educação e promoção de leitura, juntamente
+    com avaliação e pesquisa, para reconhecer a importância da leitura
+    de alto nível como uma capacidade de moldar a vida e a sociedade.
+    A educação e a promoção da leitura precisam ir além do ensino
+    de habilidades funcionais e informativas básicas para crianças em
+    idade escolar e focar o processo de desenvolvimento pessoal ao
+    longo da vida, aprimorado pela leitura. O futuro da leitura afeta
+    o futuro das nossas sociedades. Os formuladores de políticas em
+    todas as áreas precisam estar cientes disso.
+
+    Disponível em: www.publishnews.com.br.
+    Acesso em: 10 jan. 2024 (adaptado).
+
+    Esse manifesto, lançado na Feira do Livro de Frankfurt de 2023,
+    apresenta como marca desse gênero textual um caráter:""",
+        
+        "alternativas": {
+            "A": "Persuasivo, ao combater a leitura digital.",
+            "B": "Subjetivo, ao traçar um perfil ideal de leitor.",
+            "C": "Emotivo, ao defender a relevância do livro impresso.",
+            "D": "Normativo, ao definir medidas educativas necessárias.",
+            "E": "Reivindicatório, ao alertar a sociedade sobre o papel da leitura."
+        },
+    
+    "resposta": "E"
+},
+
+ {
+    "pergunta": """Leia o texto a seguir:
+
+Lima Barreto, o fato e a ficção
+
+Neste volume da Biblioteca Carioca, apresentam-se dois
+textos de Lima Barreto. O primeiro — Diário do hospício —
+combina memórias e reflexões acerca da vida no manicômio
+(e fora dele, algumas vezes), constituindo-se no diário do
+escritor, relativo ao período de 25 de dezembro de 1919 a
+2 de fevereiro de 1920, em que se encontrava internado
+no Hospício de D. Pedro II, situado na Praia Vermelha.
+
+O segundo texto — O cemitério dos vivos — apresenta caráter
+ficcional e, apesar de centrar-se em um núcleo familiar,
+remete muitas vezes às observações transcritas no Diário do
+hospício. Acrescentou-se aos dois textos um conto, Como o
+homem chegou, em que um episódio ocorrido com o autor,
+por ocasião de seu primeiro internamento, serve de base à
+fabulação; e cartas enviadas e recebidas por Lima Barreto
+quando se encontrava no hospício.
+
+MACHADO, A. L. O.; GENS, R. M. C. In: BARRETO, L. Diário do hospício;
+O cemitério dos vivos. Rio de Janeiro: Biblioteca Carioca, 1993 (adaptado).
+
+Esse trecho do prefácio do livro de Lima Barreto cumpre o propósito de:""",
+    
+    "alternativas": {
+        "A": "Tecer uma crítica aos textos.",
+        "B": "Apresentar uma introdução da obra.",
+        "C": "Evidenciar o caráter confidencial da escrita.",
+        "D": "Registrar uma experiência pessoal do autor.",
+        "E": "Informar sobre o lançamento da coletânea de textos."
+    },
+    
+    "resposta": "B"
+},
+
+{
+    "pergunta": """Leia o texto a seguir:
+
+Quando a porta se abriu, ouvi em tom baixo: “Não saia daí
+até eu voltar!”. E ela se fechou, me deixando ali, no escuro. [...]
+Até que acabaram os biscoitos e a água que levamos na
+mochila. Bateu sede, mas eu não podia sair do quartinho.
+Bateu fome, mas eu não podia sair do quartinho. Bateu vontade
+de fazer xixi, mas... descobri que tinha um microbanheiro atrás
+de outra porta branca: um vaso sanitário, um chuveiro que por
+pouco não estava sobre o vaso e, em frente aos dois, uma pia
+com um espelho na parede acima dela. Entre o espelho e a
+pia, uma prateleira com um pote, um tubo de pasta de dentes
+e uma escova dentro. Tudo no diminutivo.
+
+Quando ter uma empregada que dorme no trabalho passou
+a ser algo caro e não de muito bom-tom, os corretores de
+imóveis chamariam esse local da casa de “quarto reversível”,
+um nome para não chamar o quartinho de quartinho ou do
+que ele realmente era: um lugar para serviçais, criadas, babás,
+domésticas, amas, empregadas. Todos esses nomes que deram
+e dão até hoje a quem é “quase da família”. Um lugar onde
+estivessem ao alcance do comando de voz, do olhar, ao alcance
+das mãos... A tempo e hora, vinte e quatro horas por dia.
+
+CRUZ, E. A. Solitária. São Paulo: Cia. das Letras, 2022.
+
+Nesse fragmento, ao refletir sobre aspectos da rotina de trabalho
+da mãe, a narradora:""",
+    
+    "alternativas": {
+        "A": "Demonstra a solidão do ambiente doméstico.",
+        "B": "Revela uma concepção crítica das relações familiares.",
+        "C": "Questiona as modificações na arquitetura dos imóveis.",
+        "D": "Traça um perfil sensível do comportamento da classe média.",
+        "E": "Sugere a persistência da divisão de classe no trabalho doméstico."
+    },
+    
+    "resposta": "E"
+},
+
+    {
+    "pergunta": """Leia o texto a seguir:
+
+    "Índio eu não sou"
+
+    Não me chame de “índio” porque
+    Esse nome nunca me pertenceu
+    Nem como apelido quero levar
+    Um erro que Colombo cometeu.
+
+    Por um erro de rota
+    Colombo em meu solo desembarcou
+    E no desejo de às Índias chegar
+    Com o nome de “índio” me apelidou.
+    [...]
+
+    Chegou tarde, eu já estava aqui
+    Caravela aportou bem ali
+    Eu vi “homem branco” subir
+    Na minha Uka me escondi.
+    [...]
+
+    “Índio” eu não sou.
+    Sou Kambeba, sou Tembé
+    Sou Kokama, sou Sataré
+    Sou Guarani, sou Arawaté
+    Sou Tikuna, sou Suruí
+    Sou Tupinambá, sou Pataxó
+    Sou Terena, sou Tukano
+    Resisto com raça e fé.
+
+    KAMBEBA, M. Ay Kakyri Tama: eu moro na cidade.
+    São Paulo: Pólen, 2018 (fragmento).
+
+    Em relação aos povos originários, o texto aproxima-se ao:""",
+        
+        "alternativas": {
+            "A": "Enaltecer sua identidade.",
+            "B": "Detalhar sua formação cultural.",
+            "C": "Reconhecer o avanço de seus direitos.",
+            "D": "Defender a demarcação de suas terras.",
+            "E": "Criticar o modo como são denominados."
+        },
+        
+        "resposta": "E"
+    },
+
+{
+    "pergunta": """Leia o texto a seguir:
+
+“Minhas senhoras e meus senhores (se eventualmente
+aparecer algum, o que não é provável):
+[...]
+
+É comovedor, senhoras, esta vossa luta em prol de vossos
+jovenzinhos que ora sobem degraus na escada do saber. Soube,
+através de vossa reivindicação, que o assunto a ser debatido seriam
+as provas, nas quais vossos filhos não se saíram bem. Falaram-me,
+também, que parecia haver umas faixas onde foram bordadas
+algumas questões que não foram bem resolvidas. Senhoras,
+entendei-me: o que queremos, neste templo do saber, é elevar a
+alma humana aos píncaros do conhecimento e, senhoras, vós não
+tivestes esta áurea oportunidade, não a subtraiais a vossas crianças
+[...]. Senhoras, meu apelo especialmente movido do fundo do
+coração: sede mais severas! Obrigai vossos filhos firmemente a um
+estudo, tirai-lhes horas de folgança e arremessai-os sobre os livros!
+[...]
+
+Mais uma vez o meu renovado agradecimento e admiração por
+mães tão conscientes dos estudos de seus filhos. Enquanto houver
+mulheres como vós, bastiões de nossa sociedade, o altivo sol da
+esperança brilhará resplandecente no céu do futuro.”
+[...]
+
+Com muito vagar, as cabeças foram marcando o assentimento,
+uma a uma. As faixas jazendo esquecidas.
+
+LACERDA, N. G. Manual de tapeçaria.
+Rio de Janeiro: Philobiblion; Fundação Rio, 1986.
+
+Nesse texto, que retrata uma reunião com os responsáveis pelos
+estudantes de uma escola, a diretora usa linguagem rebuscada para:""",
+    
+    "alternativas": {
+        "A": "Esclarecer dúvidas.",
+        "B": "Intimidar os interlocutores.",
+        "C": "Enaltecer a dedicação das famílias.",
+        "D": "Buscar a precisão das informações.",
+        "E": "Mostrar preocupação com a situação."
+    },
+    
+    "resposta": "B"
+},
+
+{
+    "pergunta": """Leia o texto a seguir:
+
+Maria Manuela foi bordar no sofá. Ela ainda tinha três filhas
+para casar, e agora estava sem marido. Ainda bem que Manuela já
+tinha o Joaquim. Logo que a maldita guerra acabasse, ficavam noivos
+e casavam sem demora. Era um compromisso a menos. E depois,
+Antônio, quando voltasse, ajudaria a achar bom partido para as
+outras duas manas. Mas agora Antônio estava nos arredores de
+Porto Alegre, naquele sítio interminável que os rebeldes impunham
+à cidade. E Maria Manuela rezava por ele todos os dias, apegava-se
+às suas santas, fazia promessas complicadas, jejuava. Tinha perdido o
+marido, mas seu filho querido, esse, nem que ela tivesse de queimar
+todas as velas do Rio Grande, esse voltava para casa são e salvo.
+Pegou a agulha e recomeçou o trabalho de onde o tinha deixado na
+noite anterior. Era uma toalha de mesa, para o enxoval de Manuela.
+
+WIERZCHOWSKI, L. A casa das sete mulheres.
+Rio de Janeiro: Record, 2003.
+
+Nesse trecho do romance, o narrador expõe as preocupações
+de Maria Manuela em relação aos filhos, enfatizando valores
+relacionados à:""",
+    
+    "alternativas": {
+        "A": "Viuvez como causa de solidão da matriarca.",
+        "B": "Obrigação materna de preparar o enxoval das filhas.",
+        "C": "Visão do matrimônio como garantia de estabilidade financeira.",
+        "D": "Dependência dos filhos homens para planejar o futuro familiar.",
+        "E": "Adesão das mulheres à religiosidade como recurso de segurança."
+    },
+    
+    "resposta": "D"
+},
+
+{
+    "pergunta": """Leia o texto a seguir:
+
+A borboleta azul
+
+“Ninguém nasce borboleta”, pensou Breno. Depois disse
+baixinho: “A borboleta é um presente do tempo”. Lá fora, ela,
+a borboleta, não pensava nada disso. Ocupava-se em voar
+pela noite de árvore em árvore. Era azul e sem dúvida um dia
+havia sido lagarta. Breno tem nove anos e é uma criança,
+a lagarta é como se fosse uma borboleta criança, mas quando
+Breno for adulto vira homem e não borboleta, e homens não
+voam. Sonho de Breno é voar, seja como piloto de avião ou
+jogador de futebol. Como borboleta, Breno nunca chegou a
+pensar, tem nove anos, mas sabe que é menino e não lagarta.
+A avó de Breno sempre diz: “Lagarta queima o dedinho e come
+planta, mas vira borboleta. Ninguém nasce borboleta”. Agora o
+menino pensa e olha a borboleta na janela. “De manhã vi um
+monte de buraquinhos nas folhas”; explicaram a ele: “É coisa
+de lagarta”. Os buracos nas acerolas e goiabas eram coisa dos
+passarinhos. Isso ninguém precisou explicar, porque ele sempre
+viu os passarinhos indo bicar as frutas, menos o beija-flor, que só
+ia bicar a água no copo de flor pendurado na goiabeira. “O que
+será que borboleta come? Será que beija-flor só bebe água?”.
+Pensou muito nisso e sentiu fome. Saiu em direção à cozinha.
+
+MARTINS, G. O sol na cabeça. São Paulo: Cia. das Letras, 2018.
+
+Nesse fragmento, a estratégia que constrói a narração sob a
+perspectiva do protagonista é a:""",
+    
+    "alternativas": {
+        "A": "Recorrência de termos na forma diminutiva.",
+        "B": "Inserção de memórias no decorrer do enredo.",
+        "C": "Descrição pormenorizada do entorno do personagem.",
+        "D": "Incorporação da voz do personagem à voz do narrador.",
+        "E": "Mescla de planos narrativos nos tempos presente e passado."
+    },
+    
+    "resposta": "D"
+},
+
+{
+    "pergunta": """Leia o texto a seguir:
+
+Segundo estudiosos do nosso idioma, as gírias e os
+neologismos contemporâneos originam-se de ambientes
+segmentados, como o videogame. De vez em quando, alguns
+desses termos furam a bolha e intrigam quem não circula
+por esses espaços.
+
+Um caso em evidência atualmente é “tankar” — eu “tanko”,
+tu “tankas”. O verbo veio de um tipo de personagem popular
+em games de combate em equipe: o tank funciona como um
+escudo, permitindo ao resto do time executar suas tarefas.
+Em um ano difícil como o de 2022, a palavra se popularizou
+como sinônimo de “aguentar”, “suportar” (no sentido oposto
+surgiu “intankável”, aquilo que não pode ser suportado).
+
+“Até os anos 1980, as gírias vinham de uma cultura massiva
+mais forte e dominante, como novelas e programas de
+humor”, diz uma professora do curso de Estudos de Mídia
+da Universidade Federal Fluminense. “Havia um solo comum
+em que elas eram compartilhadas. Hoje as fontes são muito
+mais específicas e diversas”.
+
+Disponível em: https://oglobo.globo.com.
+Acesso em: 24 jan. 2024 (adaptado).
+
+De acordo com esse texto, o uso da palavra “tankar” e seus
+derivados:""",
+    
+    "alternativas": {
+        "A": "Provoca dificuldades de compreensão entre usuários de games.",
+        "B": "Produz humor fora do contexto original de sua criação.",
+        "C": "Demonstra como as gírias refletem a cultura televisiva.",
+        "D": "Marca a criação de neologismos em épocas de crise.",
+        "E": "Revela o impacto dos jogos eletrônicos na língua."
+    },
+    
+    "resposta": "E"
+},
+
+{
+    "pergunta": """Leia o texto a seguir:
+
+A gravura como obra múltipla
+
+Na experiência mexicana, a gravura se destacava como
+meio para ilustrar panfletos e material para mobilizar e
+informar os trabalhadores rurais e urbanos, de acordo com
+a luta iniciada na Revolução Mexicana. Inexistindo no Brasil
+uma “revolução em marcha”, as gravuras, realizadas pelos
+Clubes de Gravuras, limitavam-se a representar os temas rural
+e/ou urbano, focalizando o trabalhador e seu ambiente de
+trabalho, assim como suas lutas reivindicatórias. Além disso,
+as gravuras circulavam apenas entre os associados do clube
+e não amplamente, como no caso mexicano, em que eram
+distribuídas pela cidade e pelo campo.
+
+AMARAL, A. Arte para quê?: a preocupação social na arte brasileira, 1930-1970.
+São Paulo: Studio Nobel, 2003 (adaptado).
+
+Em relação à produção mexicana, as obras que circulavam pelos
+Clubes de Gravura no Brasil caracterizam uma produção que:""",
+    
+    "alternativas": {
+        "A": "Se assemelha na temática, mas se distancia na função social.",
+        "B": "Reflete a realidade do campo, com vistas a contrapor à vida na cidade.",
+        "C": "Faz referência à vivência do trabalhador, para mobilizar a classe campesina.",
+        "D": "Ganha popularidade, uma vez que difunde uma técnica advinda de outro país.",
+        "E": "Se concentra em tiragens limitadas, a fim de garantir uma circulação acessível."
+    },
+    
+    "resposta": "A"
+},
+
+ {
+    "pergunta": """Leia o texto a seguir:
+
+Alimentamos tantas expectativas de libertação para
+falarmos o que bem quisermos e para criarmos tecnologias
+e mundos novos que nos descuidamos de prestar atenção
+na ascensão dos monopólios das empresas de tecnologia,
+na construção de bolhas de informação que confirmam pontos
+de vista e na cada vez mais real possibilidade de a internet
+virar uma TV a cabo, com o já proclamado fim da neutralidade
+da rede. Tomamos um porre de otimismo. E agora estamos
+na fase de ressaca, reféns dos monopólios da internet,
+da comercialização de qualquer dado deixado na rede,
+das fake news chegando de todos os lados. Distopia pura.
+
+O cerceamento da internet por empresas privadas é um
+dos elementos principais na construção desse espírito. O que
+resta da internet hoje senão as plataformas, os softwares e
+os dispositivos dessas empresas? Para a maioria da população
+brasileira e mundial, pouco. De acordo com um dos criadores da
+ideia de realidade virtual, “a cultura digital nascente acreditava
+que tudo na internet deveria ser público, gratuito. Ao mesmo
+tempo, amávamos nossos empreendedores de tecnologia”.
+Como celebrar empreendedorismo se tudo é gratuito? Com um
+modelo baseado em publicidade. Os sites de busca e as redes
+sociais nasceram gratuitos. Mas os algoritmos se tornaram
+mais eficientes. E o que começou como propaganda não
+pode mais ser chamado de propaganda. Hoje é modificação
+de comportamento.
+
+Disponível em: https://outraspalavras.net.
+Acesso em: 24 jan. 2024 (adaptado).
+
+Ao propor uma reflexão sobre a liberdade na internet, esse
+texto destaca o(a):""",
+    
+    "alternativas": {
+        "A": "Evolução eficiente dos algoritmos.",
+        "B": "Declínio do interesse pela televisão.",
+        "C": "Impossibilidade de lidar com fake news.",
+        "D": "Importância de garantir a gratuidade da rede.",
+        "E": "Controle exercido pelas grandes corporações."
+    },
+    
+    "resposta": "E"
+},
+
+]
