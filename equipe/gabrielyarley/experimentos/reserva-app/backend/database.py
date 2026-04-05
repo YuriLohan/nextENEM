46d0e2481edbb78feea2a84de81de39f63688958
GabrielYarley <gabrielyarley549@gmail.com>
2026-04-05 20:25:12 -0300

    Login e Cadatro
    Tela de login e cadastro, ainda vou continuar a atulizar todo final de semana, estar bem rudimentar, mas é guardado as informações no BD.

---
+++ equipe/gabrielyarley/experimentos/reserva-app/backend/database.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "mysql+pymysql://root@localhost:3306/reserva_app"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(bind=engine)
+
+Base = declarative_base()
