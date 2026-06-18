539c0f45a87a3af82f77e76378cc5469f9b63bc6
Blackover3260 <afksolitario@gmail.com>
2026-06-18 10:49:13 -0300

    delete: other models nextenem

--- NextENEM/src/services/api.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export const api = axios.create({
-  baseURL: 'http://SEU_IP:8000', // ex: http://192.168.1.105:8000
-  timeout: 10000,
-  headers: { 'Content-Type': 'application/json' },
-});
